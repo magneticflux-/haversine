26b397ba82ccaab8e36af12ddbaf8eae5d3f42ca
Mitchell Skaggs <skaggsm333@gmail.com>
2019-04-22 21:09:47 -0500

    Constrain float type to real numbers only. This allows Eq, Ord, Hash traits for floats because it excludes NaN and Inf.

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,37 @@
-#[derive(Debug, Copy, Clone, PartialEq)]
+extern crate noisy_float;
+
+use noisy_float::prelude::*;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub struct Location {
-    pub latitude: f64,
-    pub longitude: f64,
+    pub latitude: R64,
+    pub longitude: R64,
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Ord, PartialOrd)]
 pub enum Units {
     Miles,
     Kilometers,
 }
 
-pub fn distance(start: Location, end: Location, units: Units) -> f64 {
+pub fn distance(start: Location, end: Location, units: Units) -> R64 {
     /// Values from Moritz, H. Journal of Geodesy (2000) 74: 128. https://doi.org/10.1007/s001900050278
     const KILOMETERS: f64 = 6371.0087714;
     const MILES: f64 = 3958.76131603933;
-    let r: f64;
+    let r: R64;
 
     match units {
-        Units::Miles => r = MILES,
-        Units::Kilometers => r = KILOMETERS
+        Units::Miles => r = r64(MILES),
+        Units::Kilometers => r = r64(KILOMETERS)
     }
 
-    let d_lat: f64 = (end.latitude - start.latitude).to_radians();
-    let d_lon: f64 = (end.longitude - start.longitude).to_radians();
-    let lat1: f64 = (start.latitude).to_radians();
-    let lat2: f64 = (end.latitude).to_radians();
+    let d_lat: R64 = (end.latitude - start.latitude).to_radians();
+    let d_lon: R64 = (end.longitude - start.longitude).to_radians();
+    let lat1: R64 = (start.latitude).to_radians();
+    let lat2: R64 = (end.latitude).to_radians();
 
-    let a: f64 = ((d_lat / 2.0).sin()) * ((d_lat / 2.0).sin()) + ((d_lon / 2.0).sin()) * ((d_lon / 2.0).sin()) * (lat1.cos()) * (lat2.cos());
-    let c: f64 = 2.0 * ((a.sqrt()).atan2((1.0 - a).sqrt()));
+    let a: R64 = ((d_lat / r64(2.0)).sin()) * ((d_lat / r64(2.0)).sin()) + ((d_lon / r64(2.0)).sin()) * ((d_lon / r64(2.0)).sin()) * (lat1.cos()) * (lat2.cos());
+    let c: R64 = r64(2.0) * ((a.sqrt()).atan2((r64(1.0) - a).sqrt()));
 
     r * c
 }
@@ -38,11 +42,11 @@ mod tests {
 
     #[test]
     fn haversine_distance_in_miles() {
-        assert_eq!(0.3412300584989182, distance(Location { latitude: 38.898556, longitude: -77.037852 }, Location { latitude: 38.897147, longitude: -77.043934 }, Units::Miles));
+        assert_eq!(r64(0.3412300584989182), distance(Location { latitude: r64(38.898556), longitude: r64(-77.037852) }, Location { latitude: r64(38.897147), longitude: r64(-77.043934) }, Units::Miles));
     }
 
     #[test]
     fn haversine_distance_in_kilometers() {
-        assert_eq!(0.549156547264883, distance(Location { latitude: 38.898556, longitude: -77.037852 }, Location { latitude: 38.897147, longitude: -77.043934 }, Units::Kilometers));
+        assert_eq!(r64(0.549156547264883), distance(Location { latitude: r64(38.898556), longitude: r64(-77.037852) }, Location { latitude: r64(38.897147), longitude: r64(-77.043934) }, Units::Kilometers));
     }
 }
